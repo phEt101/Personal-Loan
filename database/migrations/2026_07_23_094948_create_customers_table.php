f3e6f5782755342850e6a812632360b301c785c8
phEt101 <takdanaikhongta@gmail.com>
2026-07-23 17:28:05 +0700

    feat: create customers table migration with necessary fields and soft deletes

---
+++ database/migrations/2026_07_23_094948_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50)->nullable()->comment('คำนำหน้า');
+            $table->string('name', 150)->nullable()->comment('ชื่อ-นามสกุล');
+            $table->string('name_en', 150)->nullable()->comment('ชื่อ-นามสกุล (อังกฤษ)');
+            $table->date('birthdate')->nullable()->comment('วันเดือนปีเกิด');
+            $table->string('id_card', 20)->nullable()->comment('เลขบัตรประชาชน');
+            $table->string('passport', 20)->nullable()->comment('เลขหนังสือเดินทาง');
+            $table->string('nationality', 50)->nullable()->comment('สัญชาติ');
+            $table->string('phone_number', 15)->nullable()->comment('หมายเลขโทรศัพท์');
+            $table->string('email', 50)->nullable()->comment('อีเมล');
+            $table->string('address', 255)->nullable()->comment('ที่อยู่');
+            $table->string('occupation', 100)->nullable()->comment('อาชีพ');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
