773bac0850c5771043711faa4a3fd0b3d74a481c
phEt101 <takdanaikhongta@gmail.com>
2026-07-31 11:04:20 +0700

    fix: remove unused Schema import and default string length setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
         $this->loadRoutesFrom(app_path('Modules/Auth/Routes/web.php'));
         $this->loadViewsFrom(app_path('Modules/Auth/Resources/views'), 'auth');
         $this->loadTranslationsFrom(app_path('Modules/Auth/Resources/lang'), 'auth');
